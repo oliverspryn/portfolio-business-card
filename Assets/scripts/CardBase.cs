9ee0dd579c7f6c13e6feededf092575f3ee68ed8
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2015-03-24 21:40:23 -0400

    Created initial project, and added UnityVS support.

---
+++ Assets/scripts/CardBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
