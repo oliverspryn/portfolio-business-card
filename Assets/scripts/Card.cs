d6ce0ebe05961aa5346ef1444a88e998840353dd
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2015-03-30 21:55:34 -0400

    Made a rotating business card, with static images to represent both sides.

---
+++ Assets/scripts/Card.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+	#region Fields
+
+/// <summary>
+/// The number of degrees which the card will rotate on each frame update.
+/// </summary>
+	public float RotationRate = -0.05f;
+
+	#endregion
+
+	#region Methods
+
+/// <summary>
+/// This update method will slowly rotate the card along the Y-axis.
+/// </summary>
+	public void Update() {
+		gameObject.transform.Rotate(0.0f, RotationRate, 0.0f);
+	}
+
+	#endregion
+}
---
+++ Assets/scripts/Side.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Side : MonoBehaviour {
+	#region Fields
+
+/// <summary>
+/// The URL of the texture to apply to this side of the card.
+/// </summary>
+	public string URL = "https://www.google.com/images/srpr/logo11w.png";
+
+	#endregion
+
+	#region Methods
+
+/// <summary>
+/// Initializes the texture for this side of the card and loads the texture
+/// from the internet.
+/// </summary>
+/// 
+/// <returns>The object which loads the material from the internet</returns>
+	public IEnumerator Start() {
+		Renderer ren = gameObject.GetComponent<Renderer>();
+
+	//Hide the object until the texture is loaded
+		ren.enabled = false;
+
+	//Synchronously load the texture
+		WWW www = new WWW(URL);
+		yield return www;
+		ren.material.mainTexture = www.texture;
+
+	//Now show the object
+		ren.enabled = true;
+	}
+
+	#endregion
+}
